5af5157a5b8f93043fc08c5ca60060d3656bd6ac
Joyce Mok <107952129+joycemok@users.noreply.github.com>
2024-02-16 01:04:24 -0800

    Update my_spotify_queries.sql

--- my_2023_spotify_data/my_spotify_queries.sql
+++ my_2023_spotify_data/my_spotify_queries.sql
@@ -25,41 +25,57 @@ ADD COLUMN hour_played INT AS (HOUR(playTime));
 SELECT MIN(playTime) AS first_date, MAX(playTime) AS last_date
 FROM stream_history;
 
-# 2. What are my top 10 most listend to songs of 2023? 
+# 2a. What are my top 10 most listend to songs of 2023? 
 SELECT trackName, artistName, SUM(msPlayed)/60000 AS minPlayed, COUNT(trackName) as timesPlayed
 FROM stream_history
 GROUP BY trackName, artistName 
 ORDER BY minPlayed DESC
 LIMIT 10;
 
-# 3. What are my top 10 most listened to artists of 2023?
+# 2b. What are my listening habits for my top song?
+SELECT month_played, DATE(playTime) AS date_played, trackName, artistName, SUM(msPlayed)/60000 AS minPlayed, COUNT(trackName) AS timesPlayed
+FROM stream_history
+WHERE trackName = "Monsters (feat. blackbear)"
+GROUP BY trackName, artistName, month_played, date_played
+ORDER BY month_played DESC;
+
+# 3a. What are my top 10 most listened to artists of 2023?
 SELECT artistName, SUM(msPlayed) / 60000 AS minPlayed
 FROM stream_history
 GROUP BY artistName
 ORDER BY minPlayed DESC
 LIMIT 10;
 
-# 4. What are my top 3 songs from my top most listened to artists?
+# 3b. What are my top 3 songs from my top most listened to artists?
+WITH top_songs AS (
+	SELECT artistName, trackName, SUM(msPlayed) / 60000 AS minPlayed, COUNT(trackName) AS timesPlayed, RANK() OVER(PARTITION BY artistName ORDER BY SUM(msPlayed) DESC) as ranking
+	FROM stream_history
+	WHERE artistName IN ('Post Malone', 'The Band CAMINO', 'Taylor Swift', 'Quinn XCII', 'Ed Sheeran')
+	GROUP BY artistName, trackName
+)
+SELECT artistName, trackName, minPlayed, timesPlayed
+FROM top_songs
+WHERE ranking <= 3;
 
-# 5. How many minutes did I listen to music each month?
+# 4. How many minutes did I listen to music each month?
 SELECT MONTHNAME(playTime) AS months, SUM(msPlayed) / 60000 AS minPlayed
 FROM stream_history
 GROUP BY months
 ORDER BY minPlayed DESC;
 
-# 6. What are my listening habits for each hour of the day?
+# 5. What are my listening habits for each hour of the day?
 SELECT hour_played, SUM(msPlayed)/60000 AS minPlayed
 FROM stream_history
 GROUP BY hour_played
 ORDER BY hour_played ASC;
 
-# 7. What time of day did I listen to music the most?
+# 6. What time of day did I listen to music the most?
 SELECT hour_played, SUM(msPlayed)/60000 AS minPlayed
 FROM stream_history
 GROUP BY hour_played
 ORDER BY minPlayed DESC;
 
-# 8. Who are my top 5 artists each month and how many minutes did I listen to each artist?
+# 7. Who are my top 5 artists each month and how many minutes did I listen to each artist?
 WITH bymonth AS (
 	SELECT month_played, artistName, SUM(msPlayed)/60000 AS minPlayed
 	FROM stream_history
